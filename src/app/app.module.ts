6652d84f54f1bc8d3150da5ec897d5d5d2ad4672
Mr Mubikayi <31312536+mr-mubikayi@users.noreply.github.com>
2023-06-14 13:03:34 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// ReactiveForms
+import { ReactiveFormsModule } from '@angular/forms';
+
+// NgCharts
+import { NgChartsModule } from 'ng2-charts';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot({ mode: 'ios' }), ReactiveFormsModule, AppRoutingModule, NgChartsModule],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
